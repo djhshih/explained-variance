f1487dd7461fd36d4620f3277e035e7902452674
David J. H. Shih <djh.shih@gmail.com>
2023-08-23 20:38:09 +0800

    Add shape contrained generalized additive model.

--- simulate.R
+++ simulate.R
@@ -1,38 +1,30 @@
 library(MASS)
-library(caret)
+library(scam)
 
-N <- 100;
-
-sigma <- 0.3;
+# fraction of explained variance
+fev <- function(x, x.hat) {
+	ssr <- sum((x - x.hat)^2);
+	sst <- sum((x - mean(x))^2);
+	1 - ssr / sst
+}
 
 # simulate data
 
 set.seed(1334)
+
+N <- 100;
+sigma <- 0.3;
+
 x1 <- runif(N);
 
-x2 <- rnorm(N, sd = sigma);
-fev.true <- 0;
+#x2 <- rnorm(N, sd = sigma);
+#fev.true <- 0;
 
-#mu <- x1^3 + 2*x1^2 - 1;
-#x2 <- rnorm(N, mean = mu, sd = sigma);
-#fev.true <- 1 - sigma^2 / var(mu);
+mu <- -3*(x1 - 1)^2 + 5;
+x2 <- rnorm(N, mean = mu, sd = sigma);
+fev.true <- 1 - sigma^2 / var(mu);
 
 X <- matrix(c(x1, x2), ncol=2);
-
-# fit KNN regression
-
-K <- 30;
-
-fit.x1 <- knnreg(X, x1, k = K);
-fit.x2 <- knnreg(X, x2, k = K);
-
-x1.hat <- predict(fit.x1, X);
-x2.hat <- predict(fit.x2, X);
-
-X.hat <- matrix(c(x1.hat, x2.hat), ncol=2);
-
-plot(X[,1], X[,2])
-points(X.hat[,1], X.hat[,2], pch=20)
 
 # fit loess regression
 
@@ -43,11 +35,28 @@ plot(X[,1], X[,2])
 idx <- order(x1);
 lines(x1[idx], x2.hat[idx], pch=20)
 
-ssr <- sum((x2 - x2.hat)^2);
-sst <- sum((x2 - mean(x2))^2);
-
-fev <- 1 - ssr / sst;
+fev.hat <- fev(x2, x2.hat);
 
 print(fev.true)
-print(fev)
+print(fev.hat)
+
+# fit shape constrained generalized additive models
+
+k <- 5;
+fit <- scam(x2 ~ s(x1, k = k, bs = "mpi"));
+d <- data.frame(x1=x1);
+x2.hat <- predict(fit, se.fit=TRUE, d);
+
+plot(X[,1], X[,2])
+idx <- order(x1);
+lines(x1[idx], x2.hat$fit[idx], pch=20)
+# confidence band
+z <- qnorm(1 - 0.05/2);
+lines(x1[idx], x2.hat$fit[idx] - z*x2.hat$se.fit[idx], pch=20)
+lines(x1[idx], x2.hat$fit[idx] + z*x2.hat$se.fit[idx], pch=20)
+
+fev.hat <- fev(x2, x2.hat$fit);
+
+print(fev.true)
+print(fev.hat)
 
